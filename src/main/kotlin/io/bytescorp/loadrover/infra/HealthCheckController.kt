713be0e20ea0f541cc94df6404b5b945b971afe4
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-01-11 16:00:32 +0900

    feat: swagger 및 healthCheck API 추가

---
+++ src/main/kotlin/io/bytescorp/loadrover/infra/HealthCheckController.kt
@@ -0,0 +1,12 @@
+package io.bytescorp.loadrover.infra
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health-check")
+    fun healthCheck(): String {
+        return "OK"
+    }
+}
