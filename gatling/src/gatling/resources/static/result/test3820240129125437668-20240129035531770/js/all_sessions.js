11d634d8d30f659de593ea8cc05edb2780d63582
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-01-29 14:20:01 +0900

    feat: git runner 수정 2

---
+++ gatling/src/gatling/resources/static/result/test3820240129125437668-20240129035531770/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1706500534000,1],[1706500535000,1],[1706500536000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
