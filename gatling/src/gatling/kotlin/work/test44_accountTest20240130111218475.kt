f47b2b61062f0d8ea221e9c1ba8eca4b893156a8
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-01-30 11:13:31 +0900

    fix: account 없을 시, action 누락 이슈 해결

---
+++ gatling/src/gatling/kotlin/work/test44_accountTest20240130111218475.kt
@@ -0,0 +1,24 @@
+package work
+
+import io.gatling.javaapi.core.*
+import io.gatling.javaapi.core.CoreDsl.*
+import io.gatling.javaapi.http.HttpDsl.*
+
+class test44_accountTest20240130111218475: Simulation() {
+val httpProtocol = http
+    .baseUrl("http://localhost:8089/tms")
+    .inferHtmlResources()
+    .acceptEncodingHeader("gzip, deflate, br")
+    .acceptLanguageHeader("ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
+val headers_0: MutableMap<CharSequence, String> = HashMap()
+init {
+headers_0.put("UserAgent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+headers_0.put("accept","application/json, text/plain, */*")
+headers_0.put("Content-Type","application/json")
+val scn = scenario("test44_accountTest20240130111218475")
+.exec(http("request_POST").post("/auth/authentication").headers(headers_0).body(StringBody("")).check(jsonPath("$.accessToken").saveAs("accessToken")))
+.pause(1)
+.exec(http("request_GET").get("/health-check2").headers(headers_0))
+.pause(1)
+this.setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol)
+}}
---
+++ gatling/src/gatling/resources/static/result/test3820240129171409743-20240129094105531/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1706521267000,1],[1706521268000,1],[1706521269000,1],[1706521270000,1],[1706521271000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
