4d10b2bb5c82d8da2ddb4420a2ab5231d478cc1a
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-02-07 14:49:12 +0900

    fixed untracked files2

--- api/src/main/kotlin/loadrover/api/io/config/exception/NotFoundDataException.kt
+++
@@ -1,6 +0,0 @@
-package loadrover.api.io.config.exception
-
-class NotFoundDataException(exceptionCode: ExceptionCode): RuntimeException() {
-    val baseResponseCode: ExceptionCode = exceptionCode
-    override val message: String = this.baseResponseCode.message
-}
