7cc4b0d1cdf558f991e857ec1e1635f8f709d9a7
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-03-18 10:55:25 +0900

    feat: application.yml 변경3

--- api/src/main/kotlin/loadrover/api/io/config/LoadroverConfig.kt
+++ api/src/main/kotlin/loadrover/api/io/config/LoadroverConfig.kt
@@ -1,24 +1,24 @@
-package loadrover.api.io.config
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "loadrover")
-data class LoadroverConfig(
-    val output: OutputProperty,
-    val gatling: GatlingProperty
-) {
-    data class OutputProperty(
-        val classNamePrefix: String
-    )
-
-    data class GatlingProperty(
-        val path: String,
-        val source: String,
-        val work: String,
-        val workPath: String,
-        val progress: String,
-        val complete: String,
-        val result: String,
-        val imagePath: String
-    )
-}
+//package loadrover.api.io.config
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties
+//
+//@ConfigurationProperties(prefix = "loadrover")
+//data class LoadroverConfig(
+//    val output: OutputProperty,
+//    val gatling: GatlingProperty
+//) {
+//    data class OutputProperty(
+//        val classNamePrefix: String
+//    )
+//
+//    data class GatlingProperty(
+//        val path: String,
+//        val source: String,
+//        val work: String,
+//        val workPath: String,
+//        val progress: String,
+//        val complete: String,
+//        val result: String,
+//        val imagePath: String
+//    )
+//}
