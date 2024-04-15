1c14c25ce9e780758ed70916d9eb01e2fd84445a
SashaPark1 <92766548+SashaPark1@users.noreply.github.com>
2024-04-15 14:25:00 +0900

    feat: scheduler 중간 커밋

---
+++ api/src/main/kotlin/loadrover/api/io/domain/task/TaskResultScheduler.kt
@@ -0,0 +1,39 @@
+package loadrover.api.io.domain.task
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import java.time.LocalDateTime
+
+@EnableScheduling
+@SpringBootApplication
+class TaskResultScheduler(
+) {
+    private val logger = LoggerFactory.getLogger(TaskResultScheduler::class.java)
+
+    //
+    @Scheduled(cron = "0 */1 * * * *") // 매 1분으로 설정
+    fun uploadReport() {
+
+//        val progressFileList = fileUtils.searchFiles("progress")
+//        val resultFolderList = fileUtils.searchResultDirectories()
+//
+//        for (progressFile in progressFileList) {
+//            val scenarioId = progressFile.scenarioId
+//
+//            for (resultFolder in resultFolderList) {
+//                val resultFileId = resultFolder.scenarioId.replace("-\\d+".toRegex(),"")
+//
+//                if (resultFileId == scenarioId) {
+//                    fileUtils.moveJsonFile(scenarioId, loadroverConfig.gatling.progress, loadroverConfig.gatling.complete)
+//                    // result html 헤더 수정
+//                    fileUtils.copyResourceFile(resultFolder.scenarioId)
+//                    htmlUtils.reviseHtmlHeader(resultFolder.scenarioId)
+//                }
+//            }
+//        }
+        logger.debug("Check simulation result: {}", LocalDateTime.now())
+    }
+
+}
